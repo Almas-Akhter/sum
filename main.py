61651d901cd6962b2db550e6937b7ec588af2e3d
Almas-Akhter <160632064+Almas-Akhter@users.noreply.github.com>
2024-02-29 17:36:08 +0530

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-a = int(input("Enter a number: "))  # taking input from user for the first integer.
-b = int(input("Enter another number: "))   # taking input from user for the second integer.
-print('The Sum of number', a, 'and', b,'is:', (a+b))    # printing the output with proper formatting.
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+print('The Sum of number', a, 'and', b,'is:', (a+b))
